3b2f546e3beaae8e619334b4276153411b7eb57d
Hupsty <mail4hupy@aol.com>
2022-06-09 20:49:41 -0500

    still working on color changing trail

--- etch.js
+++ etch.js
@@ -1,29 +1,27 @@
-function generategrid(x = 32, y = 32) {
+function generategrid(x = 32) {
     if (x >100) {
         let x = 100;
     }
-    if (y > 100) {
-        let y = 100;
-    }
-    let height = (y/720);   
+    
+    let height = (x/720);   
     let width = (x/720);
 
     Array.from(document.querySelectorAll(".block")).forEach(e => e.remove());
     
     for (let i = 0; i < x; i++){
-        for (let j = 0; j < y; j++) {
+        for (let j = 0; j < x; j++) {
             const div = document.createElement('div');
             div.classList.add("blockdefault", "block"); 
-            div.style = "display: flex; height: calc((100% / " + y + ")); width: calc((100% / " + x + "));"
+            div.style = "display: flex; height: calc((100% / " + x + ")); width: calc((100% / " + x + "));"
             container.appendChild(div);
         }
     }
 }
 
 function resize(){
-    let resizeinput = prompt("input width and height", "32 32");
+    let resizeinput = prompt("input size (1-100)", "32");
     resizeinput = resizeinput.split(" ");
-    generategrid(resizeinput[0], resizeinput[1]);
+    generategrid(resizeinput[0]);
 }
 
 function erase(){
@@ -47,22 +45,58 @@ function editable(){
     blocks.forEach((block) => {
         block.classList.add("editable");
         block.onmouseenter = function (){
+            rgbrandomizer();
+            updatecolorrule();
             this.classList.add(cursorStatusAdd);
             this.classList.remove(cursorStatusRemove);
-        }
+        };
     });
-};
+}
 
 function uneditable(){
     const blocks = document.querySelectorAll(".block");
     blocks.forEach((block) => {
         if (!block.classList.contains("blockhovered")) {
             block.classList.remove("editable");
-            block.replaceWith(block.cloneNode(false));
         };
+        block.replaceWith(block.cloneNode(false));
     });
-};
+}
 
+function rgbrandomizer(){
+    rgbr = Math.floor(Math.random() * 257);
+    rgbg = Math.floor(Math.random() * 257);
+    rgbb = Math.floor(Math.random() * 257);
+    rgb = "(" + rgbr + ", " + rgbg + ", " + rgbb + ")";
+}
+
+// function changehoverproperty(){
+//     colorrule.cssText = ".blockhovered.editable { background-color: " + rgb + ";}"
+// }
+
+// function colorruleinitialize(){
+//     for (let i = 0; i < stylesheet.cssRules.length; i++){
+//         if (stylesheet.cssRules[i].selectorText === ".blockhovered.editable"){
+//             colorrule = stylesheet.cssRules[i];
+//         };
+//     };
+// }
+
+function updatecolorrule (){
+    stylesheet.deleteRule(1);
+    stylesheet.insertRule(`.blockhovered.editable {background-color: ${rgb} yellow}`, 1);
+}
+
+
+let stylesheet = document.styleSheets[0];
+
+let colorrule;
+
+let rgbr = 100;
+let rgbg = 100;
+let rgbb = 100; 
+
+let rgb = "rgb(" + rgbr + ", " + rgbg + ", " + rgbb + ")";
 
 let container = document.querySelector(".container");
 
@@ -70,5 +104,7 @@ const resizeButton = document.querySelectorAll("#resize");
 
 let cursorStatusAdd = "blockhovered";
 let cursorStatusRemove = "";
+
+// colorruleinitialize();
 
 generategrid();
